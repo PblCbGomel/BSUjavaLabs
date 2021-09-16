fe5a9240643730123b8a2afc4413af880a99afd5
Evgeniy Shevcov <yevgeniy.shevtsov.pblcb@mail.ru>
2021-09-16 10:41:32 +0300

    Add second lab

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,85 +1,32 @@
 package com.company;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.MathContext;
-import java.math.RoundingMode;
-import java.util.Scanner;
-
-class Functions {
-
-  public static BigDecimal power(BigDecimal num, BigInteger pow) {
-    BigDecimal result = BigDecimal.ONE;
-
-    if (result.compareTo(BigDecimal.ZERO) == 0) {
-      if (pow.compareTo(BigInteger.ZERO) == 0) {
-        return BigDecimal.ONE;
-      } else {
-        return BigDecimal.ZERO;
-      }
-    } else if (pow.compareTo(BigInteger.ZERO) < 0) {
-      pow = pow.multiply(BigInteger.valueOf(-1));
-      for (int i = 0; pow.compareTo(BigInteger.valueOf(i)) > 0; ++i) {
-        result = result.multiply(num);
-      }
-      result = BigDecimal.ONE.divide(result);
-    } else {
-      for (int i = 0; pow.compareTo(BigInteger.valueOf(i)) > 0; ++i) {
-        result = result.multiply(num);
-      }
-    }
-
-    return result;
-  }
-
-  public static BigDecimal factorial(BigInteger num) {
-    BigDecimal result = BigDecimal.ONE;
-
-    if (num.compareTo(BigInteger.ONE) > 0) {
-      for (int i = 2; num.compareTo(BigInteger.valueOf(i)) >= 0; ++i) {
-        result = result.multiply(BigDecimal.valueOf(i));
-      }
-    }
-
-    return result;
-  }
-
-  public static BigDecimal sinDivX(BigInteger epsPower, BigDecimal realNumber, BigDecimal x) {
-    BigDecimal result = BigDecimal.valueOf(0);
-    BigDecimal eps = power(BigDecimal.valueOf(10), epsPower.multiply(BigInteger.valueOf(-1)));
-    MathContext mc = new MathContext(epsPower.intValue() * 10);
-
-    for (BigInteger i = BigInteger.ZERO; ((result.subtract(realNumber)).abs()).compareTo(eps) > 0;
-        i = i.add(BigInteger.TWO)) {
-        result = result.add(power(BigDecimal.valueOf(-1), i.divide(BigInteger.TWO))
-          .multiply(power(x, i).divide(factorial(i.add(BigInteger.ONE)), mc)));
-    }
-
-    return result;
-  }
-
-}
+import java.util.Formatter;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        Formatter frm1 = new Formatter();
+        frm1.format("%o %x", 16, 16);
+        System.out.println("Point 1: " + frm1);
 
-    System.out.print("Input eps power: ");
-    BigInteger k = in.nextBigInteger();
+        Formatter frm2 = new Formatter();
+        frm2.format("%f", 16.);
+        System.out.println("Point 2: " + frm2);
 
-    System.out.print("Input x: ");
-    BigDecimal x = in.nextBigDecimal();
+        Formatter frm3 = new Formatter();
+        frm3.format("%20f", 16.12);
+        System.out.println("Point 3: " + frm3);
 
-    BigDecimal realResult = BigDecimal.valueOf(Math.sin(x.doubleValue())/x.doubleValue());
+        Formatter frm4 = new Formatter();
+        frm4.format("%.6f", 16.123456789);
+        System.out.println("Point 4: " + frm4);
 
-    BigDecimal result = Functions.sinDivX(k, realResult, x);
+        Formatter frm5 = new Formatter();
+        frm5.format("%+.3f %n%,.06f %n%-7d %n%(.3f %n% f", 16.123, 4325123456.7891, 123, -12., -13.);
+        System.out.println("Point 5:\n" + frm5);
 
-
-    System.out.println("Result: " + String.format("%." + (k.add(BigInteger.ONE)) + "f", result));
-    System.out.println("Real result: " + String.format("%." + (k.add(BigInteger.ONE)) + "f", realResult));
-    System.out.println("Error: " + String.format("%." + (k.add(BigInteger.ONE)) + "f",result.subtract(realResult).abs()));
-    System.out.println("Result in exponent performance: " + String.format("%e", result));
-    System.out.println("Result in 16 system: " + String.format("%a", result.doubleValue()));
-  }
+        Formatter frm6 = new Formatter();
+        frm6.format("%3$d %5$d %4$d %1$d %2$d", 1, 2, 3, 4, 5);
+        System.out.println("Point 6: " + frm6);
+    }
 }
