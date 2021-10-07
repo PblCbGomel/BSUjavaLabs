6039f6edb570dd5a49aec18a57699582f709ae05
Evgeniy Shevcov <yevgeniy.shevtsov.pblcb@mail.ru>
2021-10-07 10:07:59 +0300

    Added fourth lab

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,189 +1,136 @@
 package com.company;
 
-import java.text.DecimalFormat;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
-
-class DoubleMatrixWork {
-
-  public static void inputMatrix(Double matrix[][], int matrixSize) {
-    Scanner in = new Scanner(System.in);
-
-    for (int i = 0; i < matrixSize; ++i) {
-      for (int j = 0; j < matrixSize; ++j) {
-        System.out.print("Input [" + i + "][" + j + "] element in matrix: ");
-        matrix[i][j] = in.nextDouble();
-      }
-    }
-  }
-
-  public static void randomFilling(Double matrix[][], int matrixSize) {
-    for (int i = 0; i < matrixSize; ++i) {
-      for (int j = 0; j < matrixSize; ++j) {
-        matrix[i][j] = Math.random() * 100;
-        System.out.println("[" + i + "][" + j + "] element in matrix = " + matrix[i][j]);
-      }
-    }
-  }
-
-  public static void outputMatrix(Double matrix[][], int matrixSize) {
-    for (int i = 0; i < matrixSize; ++i) {
-      for (int j = 0; j < matrixSize; ++j) {
-        System.out.print(matrix[i][j] + " ");
-      }
-      System.out.println();
-    }
-  }
-
-  public static void formatOutputMatrix(Double matrix[][], int matrixSize) {
-    for (int i = 0; i < matrixSize; ++i) {
-      for (int j = 0; j < matrixSize; ++j) {
-        if (i == 0) {
-          DecimalFormat percentFormat = new DecimalFormat("¤");
-          System.out.print(percentFormat.format(matrix[i][j]) + " ");
-        } else if (i == 1) {
-          DecimalFormat percentFormat = new DecimalFormat("%");
-          System.out.print(percentFormat.format(matrix[i][j]) + " ");
-        } else {
-          DecimalFormat percentFormat = new DecimalFormat("###.##");
-          System.out.print(percentFormat.format(matrix[i][j]) + " ");
-        }
-      }
-      System.out.println();
-    }
-  }
-
-  public static boolean isEqual(Double matrix1[][], Double matrix2[][], int matricesSize) {
-    for (int i = 0; i < matricesSize; ++i) {
-      for (int j = 0; j < matricesSize; ++j) {
-        if (Math.abs(matrix1[i][j] - matrix2[i][j]) > Math.ulp(1.0)) {
-          return false;
-        }
-      }
-    }
-
-    return true;
-  }
-
-  public static void sideDiagonalTransposition(Double matrix[][], int matrixSize) {
-    for (int i = 0; i < matrixSize; ++i) {
-      for (int j = 0; j < matrixSize - i - 1; ++j) {
-        matrix[i][j] += matrix[matrixSize - j - 1][matrixSize - i - 1];
-        matrix[matrixSize - j - 1][matrixSize - i - 1] =
-            matrix[i][j] - matrix[matrixSize - j - 1][matrixSize - i - 1];
-        matrix[i][j] = matrix[i][j] - matrix[matrixSize - j - 1][matrixSize - i - 1];
-      }
-    }
-  }
-
-  public static void mainDiagonalTransposition(Double matrix[][], int matrixSize) {
-    for (int i = 0; i < matrixSize; ++i) {
-      for (int j = 0; j < i; ++j) {
-        matrix[i][j] += matrix[j][i];
-        matrix[j][i] = matrix[i][j] - matrix[j][i];
-        matrix[i][j] = matrix[i][j] - matrix[j][i];
-      }
-    }
-  }
-
-  public static int compare(double a, double b) {
-    if (a > b) {
-      return 1;
-    } else if (a < b) {
-      return -1;
-    } else {
-      return 0;
-    }
-  }
-
-  public static int backCompare(double a, double b) {
-    if (a > b) {
-      return -1;
-    } else if (a < b) {
-      return 1;
-    } else {
-      return 0;
-    }
-  }
-
-  public static void sortLastLine(Double matrix[][], int matrixSize) {
-    Arrays.sort(matrix[matrixSize - 1], (a, b) -> backCompare(a, b));
-  }
-
-  public static boolean binarySearchInLine(Double matrix[][], double num, int lineNumber) {
-    Arrays.sort(matrix[lineNumber], (a, b) -> compare(a, b));
-    return Arrays.binarySearch(matrix[lineNumber], num) != -1;
-  }
-}
+import java.util.StringTokenizer;
 
 public class Main {
+
   public static void main(String[] args) {
+    String defaultString = new String();
+    String separators = new String();
+
     Scanner in = new Scanner(System.in);
+    System.out.print("Input default string: ");
+    defaultString = in.nextLine();
 
-    System.out.print("Input size of arrays: ");
-    int size = in.nextInt();
+    System.out.print("Input separators: ");
+    separators = in.nextLine();
 
-    System.out.println("Matrix 1: ");
-    Double[][] mat1 = new Double[size][size];
-    DoubleMatrixWork.inputMatrix(mat1, size);
+    System.out.println("===========================Result===========================");
 
-    System.out.println("Matrix 2: ");
-    Double[][] mat2 = new Double[size][size];
-    DoubleMatrixWork.inputMatrix(mat2, size);
-
-    System.out.println("Matrix 1: ");
-    DoubleMatrixWork.outputMatrix(mat1, size);
-    System.out.println("===========================================");
-    System.out.println("Matrix 2: ");
-    DoubleMatrixWork.outputMatrix(mat2, size);
-
-    System.out.println("===========================================");
-    System.out.println("================Reulst work================");
-    System.out.println("===========================================");
-
-    if (DoubleMatrixWork.isEqual(mat1, mat2, size)) {
-      System.out.println(
-          "The second path of a finite transposition number CAN be obtained from the first matrix.");
+    ArrayList<String> num16x = new ArrayList<String>();
+    ArrayList<String> notNum16x = new ArrayList<String>();
+    String[] words;
+    if(separators.length() == 1) {
+      words = defaultString.split(separators);
     } else {
-      DoubleMatrixWork.mainDiagonalTransposition(mat1, size);
-      if (DoubleMatrixWork.isEqual(mat1, mat2, size)) {
-        System.out.println(
-            "The second path of a finite transposition number CAN be obtained from the first matrix.");
-      } else {
-        DoubleMatrixWork.mainDiagonalTransposition(mat1, size);
-        DoubleMatrixWork.sideDiagonalTransposition(mat1, size);
-        if (DoubleMatrixWork.isEqual(mat1, mat2, size)) {
-          System.out.println(
-              "The second path of a finite transposition number CAN be obtained from the first matrix.");
-        } else {
-          DoubleMatrixWork.mainDiagonalTransposition(mat1, size);
-          if (DoubleMatrixWork.isEqual(mat1, mat2, size)) {
-            System.out.println(
-                "The second path of a finite transposition number CAN be obtained from the first matrix.");
-          } else {
-            System.out.println(
-                "The second path of the finite transposition number CANNOT be obtained from the first matrix.");
-          }
+      StringTokenizer st = new StringTokenizer(defaultString, separators);
+      words = new String[st.countTokens()];
+      for(int i = 0; st.hasMoreTokens(); ++i) {
+        String temp = st.nextToken();
+        if(temp != null) {
+          words[i] = temp;
         }
       }
     }
 
-    System.out.println("===========================================");
-    System.out.println("Matrix 2 format output: ");
-    DoubleMatrixWork.formatOutputMatrix(mat2, size);
+    for (int i = 0; i < words.length; ++i) {
+      boolean is16xNumber = true;
+      for (int j = 0; j < words[i].length(); ++j) {
+        int codeChar = words[i].codePointAt(j);
+        if ((codeChar > '9' || codeChar < '0') && (codeChar < 'A' || codeChar > 'F')
+            && ((codeChar < 'a' || codeChar > 'f'))) {
+          is16xNumber = false;
+          break;
+        }
+      }
 
-    System.out.println("===========================================");
-    System.out.println("Matrix 2 after sort last line: ");
-    DoubleMatrixWork.sortLastLine(mat2, size);
-    DoubleMatrixWork.outputMatrix(mat2, size);
+      if (is16xNumber == true) {
+        num16x.add(words[i].toLowerCase(Locale.ROOT));
+      } else {
+        notNum16x.add(words[i]);
+      }
+    }
 
-    System.out.println("===========================================");
-    System.out.println("===========Matrix2 binary search===========");
-    System.out.print("Input number to search: ");
-    double num = in.nextDouble();
-    System.out.print("Input line number: ");
-    int lineNum = in.nextInt();
-    System.out.print("Number " + num + " is in " + lineNum + " line: " +
-        DoubleMatrixWork.binarySearchInLine(mat2, num, lineNum));
+    System.out.print("Hex number in string: ");
+    String num16xStr = "";
+    for (String s : num16x) {
+      num16xStr += s + " ";
+    }
+    System.out.println(num16xStr);
+
+    System.out.print("Not hex number in string: ");
+    String notNum16xStr = "";
+    for (String s : notNum16x) {
+      notNum16xStr += s + " ";
+    }
+    System.out.println(notNum16xStr);
+
+    System.out.print("Number with 1 more than 0: ");
+    for (String s : num16x) {
+      int counter1 = 0;
+      int counter0 = 0;
+      for(int i = 0; i < s.length(); ++i) {
+        if(s.charAt(i) == '1') {
+          ++counter1;
+        }
+        if (s.charAt(i) == '0') {
+          ++counter0;
+        }
+      }
+
+      if (counter1 > counter0) {
+        System.out.print(s + " ");
+      }
+    }
+    System.out.println();
+
+    String randomNum = Integer.toHexString((int) (Math.random() * 1000));
+    StringBuffer defaultStringBuffer = new StringBuffer(defaultString);
+
+    System.out.print("Input place to insert random number: ");
+    int place = in.nextInt();
+
+    defaultStringBuffer.insert(place, " " + randomNum + " ");
+
+    defaultString = defaultStringBuffer.toString().replaceAll("  ", " ");
+    System.out.print("Added random number: ");
+    System.out.println(defaultString);
+
+    for (int i = 0; i < notNum16x.size(); ++i) {
+      boolean isHaveLatLetter = true;
+      for (int j = 0; j < notNum16x.get(i).length(); ++j) {
+        char c = notNum16x.get(i).charAt(j);
+        if ((c < 'A' || c > 'Z') && (c < 'a' || c > 'z')) {
+          isHaveLatLetter = false;
+          break;
+        }
+      }
+
+      if (isHaveLatLetter == true) {
+        if (!notNum16x.isEmpty()) {
+          defaultString = defaultString.replace(notNum16x.get(i), "");
+        }
+        defaultString = defaultString.replaceAll("  ", " ");
+        break;
+      }
+    }
+
+    System.out.print("Default string after delete 1st lexeme with latin letters: ");
+    System.out.println(defaultString);
+
+    System.out.print("Input num P: ");
+    Scanner cin = new Scanner(System.in);
+    String P = cin.nextLine();
+
+    System.out.println("Position number " + P + " is " + defaultString.indexOf(P + " "));
+
+    if (!num16x.isEmpty()) {
+      defaultString = defaultString.replace(num16x.get(0), "");
+    }
+    defaultString = defaultString.replaceAll("  ", " ");
+    System.out.println("Default string after delete 1st hex number: " + defaultString);
   }
 }
